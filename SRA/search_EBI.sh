1db2743296d3b1f732f5e30ffb9189629828acc9
Checunmily <100115060+Checunmily@users.noreply.github.com>
2025-08-28 13:55:52 +0800

    Update search_EBI.sh

--- SRA/search_EBI.sh
+++ SRA/search_EBI.sh
@@ -3,6 +3,6 @@
 awk -F, -v OFS="\t" '$4 == 0' sra_runinfo.txt > spots0.txt
 ids=$(cut -d, -f 1 spots0.txt)
 for acc in $ids; do
-    curl -s "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=${acc}&result=read_run&fields=run_accession,study_accession,sample_accession,scientific_name,tax_id,fastq_ftp,sra_ftp,submitted_ftp&format=tsv" | tail -n +2 >> spots0.tsv
+    curl -s "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=${acc}&result=read_run&fields=run_accession,study_accession,sample_accession,scientific_name,tax_id,fastq_ftp,sra_ftp,submitted_format,submitted_ftp&format=tsv" | tail -n +2 >> spots0.tsv
     sleep 0.5
 done
