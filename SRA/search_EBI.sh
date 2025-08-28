4622943ab7e6134456cfa347f62b1203072985ff
Checunmily <100115060+Checunmily@users.noreply.github.com>
2025-08-28 13:53:43 +0800

    Create search_EBI.sh

---
+++ SRA/search_EBI.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# We may need to check those files had not loaded by NCBI but on EBI, especially those with pod5 format.
+awk -F, -v OFS="\t" '$4 == 0' sra_runinfo.txt > spots0.txt
+ids=$(cut -d, -f 1 spots0.txt)
+for acc in $ids; do
+    curl -s "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=${acc}&result=read_run&fields=run_accession,study_accession,sample_accession,scientific_name,tax_id,fastq_ftp,sra_ftp,submitted_ftp&format=tsv" | tail -n +2 >> spots0.tsv
+    sleep 0.5
+done
